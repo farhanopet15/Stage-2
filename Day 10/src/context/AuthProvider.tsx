109a920209c8406eb4482cb259b04104e4a45329
farhanopet15 <154978940+farhanopet15@users.noreply.github.com>
2025-09-06 10:14:24 +0700

    Add files via upload

---
+++ Day 10/src/context/AuthProvider.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+function setCookie(name: string, value: string, days: number) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
+  return null;
+}
+
+function deleteCookie(name: string) {
+  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;`;
+}
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [token, setToken] = useState<string | null>(() => getCookie("token"));
+
+  const login = (token: string) => {
+    setCookie("token", token, 7);
+    setToken(token);
+  };
+
+  const logout = () => {
+    deleteCookie("token");
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
